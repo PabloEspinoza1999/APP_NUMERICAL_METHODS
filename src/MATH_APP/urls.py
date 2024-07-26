0cfcf16dbb0bc1cab5f500c4323d6af0cf8bd0f0
Carlos Mairena <carlosmiguelmairena@gmail.com>
2024-07-26 09:36:48 -0600

    fix: refactor indentations

--- src/MATH_APP/urls.py
+++ src/MATH_APP/urls.py
@@ -2,12 +2,11 @@ from django.urls import path
 from .Controllers import NewtonController,SecanteController
 
 urlpatterns = [
-    path('', NewtonController.Index, name='Index'),
-    path('UserManual',NewtonController.UserManual,name='UserManual'),
-    path('ShowNewtonRaphson',NewtonController.ShowNewtonRaphson,name='ShowNewtonRaphson'),
-    path('SaveNewtonRaphson',NewtonController.SaveNewtonRaphson,name='SaveNewtonRaphson'),
-    path('ShowSecante',SecanteController.ShowSecante,name='ShowSecante'),
-    path('SaveSecante',SecanteController.SaveSecante,name='SaveSecante'),
+    path('',                  NewtonController.Index,             name='Index'             ),
+    path('UserManual',        NewtonController.UserManual,        name='UserManual'        ),
+    path('ShowNewtonRaphson', NewtonController.ShowNewtonRaphson, name='ShowNewtonRaphson' ),
+    path('SaveNewtonRaphson', NewtonController.SaveNewtonRaphson, name='SaveNewtonRaphson' ),
+    path('ShowSecante',       SecanteController.ShowSecante,      name='ShowSecante'       ),
+    path('SaveSecante',       SecanteController.SaveSecante,      name='SaveSecante'       ),
 
-    # HERE ANOTHER URLS
 ]
